b3f8631e6ec84d9ae71614f1b1965400e8ef4ab7
sudha2307 <81276604+sudha2307@users.noreply.github.com>
2024-12-20 15:29:16 +0530

    Update scrape_results.py

--- scrape_results.py
+++ scrape_results.py
@@ -8,7 +8,7 @@ def get_results(reg_no, exam):
         '__VIEWSTATEGENERATOR': '7C3C6012',
         '__EVENTVALIDATION': '/wEdAARzN7bZtmqtQXfSWIF0CIprZS6BASrBkr5QeAzZHQV1+txSYZLFsAialTI1fBLjIvnN+DvxnwFeFeJ9MIBWR693ivjs57FeIsSCjQoYF9sSNQrUVAY=',
         'TxtRegno': reg_no,
-        'CMbExam': Nov 2024,
+        'CMbExam': exam,
         'Button1': 'Submit'
     }
 
